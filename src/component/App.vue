80329d607c6480957af0eac88b0ee14271221386
ws402728767 <402728767@qq.com>
2018-01-12 21:34:09 +0800

    使用新GIT提交

--- src/component/App.vue
+++ src/component/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div>
         <header></header>
-
         <!-- 根组件会有一些子组件, 我把这些子分为两种: 一种是子页面, 一种是页面布局结构 -->
         <app-login></app-login>
         <app-regieter></app-regieter>
@@ -19,5 +18,4 @@
 </script>
 
 <style scoped>
-
 </style>
